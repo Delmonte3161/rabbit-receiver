1ae05c849260a99b028dc6620d14a55f7b3b7a4e
Saus <dsaus@cerner.com>
2018-09-12 09:06:03 -0500

    Initial

--- src/main/java/com/cerner/cts/oss/rabbit/receiver/Consumer.java
+++ src/main/java/com/cerner/cts/oss/rabbit/receiver/Consumer.java
@@ -4,8 +4,6 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Component;
 
-import com.cerner.cts.oss.mass.ingest.contract.IngestionEvent;
-
 import lombok.extern.slf4j.Slf4j;
 
 /**
